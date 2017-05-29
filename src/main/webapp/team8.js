d4291104346cd8325f665832c5f3ef692a1cfb6e
sgy17 <pruce.yaragatti@gmail.com>
2017-05-29 09:52:20 +0530

    added bootstrap interface

--- src/main/webapp/team8.js
+++ src/main/webapp/team8.js
@@ -1,167 +1,77 @@
 
-function validate_user(user_name, user_password){
-          
-            $.ajax({type: "POST",
-                    url: "",
-                    dataType: 'json',
-                    async: false,
-                    data: '{"userName": "' + user_name + '", "password" : "' + user_password + '"}',
-                    success: function(data, textStatus, JqXHR) {
-                        alert('User: '+ data.user + 'authenticated: '+ data.authenticated);
 
-                    }
+// document ready 
+$(document).ready(function(){
 
-                });
-        
-    }
+     $("#main-home").click(function(){
+         $( "div[class='container']" ).hide();
+         $("#blogger-main").show();
 
-function send_register_data(FirstName, LastName, UserId, PasswordEntered, PasswordRepeated, Hint, DOB, EmailId, Agreement){
-
-                                alert("Registering user  " + FirstName)
-                        
-            $.ajax({type: "POST",
-                    url: "",
-                    dataType: 'json',
-                    async: false,
-                    data: '{ "userFirstName": "' + FirstName + '",\
-                             "userLastName":  "' + LastName + '",\
-                             "userLoginId": "' + UserId + '",\
-                             "userPassword" : "' + PasswordEntered + '",\
-                             "userConfirmPassword": "' + PasswordRepeated + '",\
-                             "userHint": "' + Hint + '",\
-                             "userDob" : "' + DOB + '",\
-                             "userEmailId": "' + EmailId + '",\
-                             "userAgree": "' + Agreement + '" \
-                            }',
-            
-                    success: function(data, textStatus, JqXHR) {
-                        alert('User: '+ data.user + 'Registered successfully');
-                    }
-                    
-            });
-}
-
-
-$(document).ready(function(){  
-
-   $("#blogMainPage_sub1").show();
-
-    $("#signupBtnM").click(function(){
-        $("#signupForm").show();
-        $("#LoginSuccess").hide();
-        $("#headline").hide(); 
-        $("#loginForm").hide();
-        $("#blogMainPage_sub1").hide();
-        
-    });
-    
-    $("#MainLink").click(function(){
-        $("#headline").show();
-        $("#loginForm").hide();
-         $("#signupForm").hide();
-         $("#forgotpassForm").hide();
-         $("#blogMainPage_sub1").hide();
-         
-    });
-
-    $("#signinBtnM").click(function(){
-       $("#loginForm").show();
-       $("#headline").hide();
-       $("#signupForm").hide();
-       $("#forgotpassForm").hide();  
-       $("#LoginSuccess").hide()
-
-       $("#blogMainPage_sub1").hide();
-      
-    });
-
-    $("#forgotpBtn").click(function(){
-        $("#forgotpassForm").show();
-
-       $("#loginForm").hide();
-       $("#headline").hide();
-       $("#signupForm").hide();
-       $("#LoginSuccess").hide();
-       $("#blogMainPage_sub1").hide();
-       
-        
-    });
-
-    /* sign in user */
-
-    $("#signinBtn").click(function(){
-
-                alert("Processing Login");
-                var uname = $("#userid").val();
-                var passwd = $("#passwd").val();
-                $("#loginForm").hide();
-               
-              $("#blogMainPage_sub1").hide();
-              var logindata = {
-                  "user" : uname,
-                  "passowrd" : passwd
-              };
-              $.ajax({
-                  url: 'rest/user/login',
-                  type : 'post',
-                  dataType: 'json',
-                  contentType: "application/json; charset=utf-8",
-                  success : function(data) {
-                      $("#LoginSuccess").show();
-                  },
-                  data : JSON.stringify(logindata)
-
-              });
-               /* validate_user(uname, passwd);*/
-    });
-
-    /* register user, val() not value */
-
-
-    $("#registerBtn").click(function(){
-        var FirstName = $("#ufname").val();
-        var LastName  = $("#ulname").val();
-        var UserId    = $("#ucuid").val();
-        var PasswordEntered = $("#ucpass").val();
-        var PasswordRepeated = $("#ucfpass").val();
-        var Hint = $("#uhint").val();
-        var DOB = $("#udob").val();
-        var EmailId = $("#uemail").val();
-        var Agreement = $("#uagree").val();
-        send_register_data(FirstName, LastName, UserId, PasswordEntered, PasswordRepeated, Hint, DOB, EmailId, Agreement);
-
-
-    });
-
-/* 
-  populate blog on page load */
-
-$(function() {
-    for(i=0;i<=50;i++) {   
-      $('#TableMain').append('<tr><td><a href="#">How to write Blogs and jist on it' + i + '</td></tr>');
-    }
-});
-
-/*  populate blog entries on headline click */
-
-    $("#MainLink").on('click', function() {
-   
-         $("#blogMainPage_sub1").show();
-         for(i=0;i<=50;i++) {   
+         for(i=0;i<=10;i++) {   
              tv='How to write Blogs and jist on it'
-            $('#TableMain').append('<tr><td>'+tv + i + '</td></tr>');
-        }
+            $('#table-main-page').append('<tr><td> <blockquote>'+tv + i +'<footer>'+ i + '</footer></blockquote></td></tr>');
+            $('#row-id').append('<div class="col-sm-4"><h3>'+ i +'</h3><p>' + tv + '</p></div>');
+         }
+  
+      });
+
+    // login
+
+    $("#login-id").click(function(){
+        $( "div[class='container']" ).hide();
+        $("#login-blogger").show();
+        
     });
 
-    $("#blogSearch").click(function(){
+    // login end
 
-
+    $("#register-id").click(function(){
+        $( "div[class='container']" ).hide();
+        $("#register-blogger").show();
+    
     });
 
-}); /* end of doc ready */
+    $("#forgot-pass-btn").click(function(){
+        $( "div[class='container']" ).hide();
+        $("#forgot-password").show();
+       
+    });
+
+   // search
+   $("#search-btn-id").click(function(){
+      alert("searching for  "+ $("#search-item").val())
+      $( "div[class='container']" ).hide();
+      $("#blogger-main").show();
+   });
+
+   //  new blog selected
+   $("#newblog-id").click(function(){
+      // if user loged in then allow else ask user to log
+       alert("please login to create new blog")
+       //$("#login-blogger").show();
+       $( "div[class='container']" ).hide();
+    if(0){
+         $("#login-blogger").show();
+    } else {
+       $("#user-id").show();
+       $("#new-post-blogger").show();
+   }
+       
+   });
+
+}); // ready end 
 
 
 
 
-
-
+     /*$("#register-blogger").hide();
+         $("#blogger-main").show();
+         $("#login-blogger").hide();
+         $("#main-page-blogger").hide();
+         $("#new-post-blogger").hide();
+         $("#register-blogger").hide();
+         $("#blogger-main").show();
+         $("#login-blogger").hide();
+         $("#main-page-blogger").hide();
+         $("#new-post-blogger").hide();*/
+         
